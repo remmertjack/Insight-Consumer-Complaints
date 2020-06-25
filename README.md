d9954117dcbc8a2398c0687acd695cdd580cb8bf
remmertjack <remmertjack@gmail.com>
2020-06-25 19:31:30 -0400

    ReadMe and minor edits

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# Insight Consumer Complaints
- 
+# Insight Data Engineering: Consumer Complaints Challenge
+******
+
+This GitHub repository contains my solution for the Fall 2020 Insight Data Engineering Fellowship [coding challenge](https://github.com/insightdatascience/consumer_complaints).
+
+## Problem and Objective
+The federal government provides a way for consumers to file complaints against companies regarding different financial products, such as payment problems with a credit card or debt collection tactics. This challenge will be about identifying the number of complaints filed and how they're spread across different companies.
+
+Thus, given a comma separated file of consumer complaints (which contains information such as date recieved, company, product, consumer notes, company info such as zip code and state, status of complaint), output an aggregated file that contains various stats for a product-year combination such as total number of complaints, number of unique companies per proudct-year, and the highest percentage of complaints per product-year.
+
+For example, the script within will create a csv in the following format:
+```
+"credit reporting, credit repair services, or other personal consumer reports",2019,3,2,67
+"credit reporting, credit repair services, or other personal consumer reports",2020,1,1,100
+debt collection,2019,1,1,100
+```
+
+For more informaiton on the data, [click here](https://cfpb.github.io/api/ccdb/fields.html).
+
+## Process
+The `consumer_complaints.py` script takes an input and output file path and then executes a wrapper function, `compile_consumer_complaints`.
+
+This wrapper function, first reads the input file line by line, cleans the data by removing excess commas and quotes and replacing them with semi-colons, and then creates a dictionary containing each product-year combination and an inner dictionary containting a company and the number of complaints filed against them for said product-year. The program will print a *Progress Report* for every million lines and once more  when the program has finished reading every record.
+
+Next, the `product_stats` function runs, executed within `compile_consumer_complaints`, to compile the statistics mentioned above and creates a dictionary containing the product-year combination and the required stats.  This is performed with a *for loop* over each product-year combination and sums the total number of complaints, counts the total number of unique companies per proudct-year using the length of said inner dictionary, and finds the highest percentage of complaints per product-year by taking the maxium value, dividng by the sum of total complaints, and rounded as required.
+
+Finally, the `write_output_file` function, executed within `compile_consumer_complaints`, writes the data to a csv given the output file. First, it builds the line using 
+string concatenation based on the output from `product_stats` and then it writes it to said file.
+
+For more comments on each individual function, please see the code itself.
+
+## Instructions
+To execute the script, move to the main directory of the project and run the following in the terminal:
+```
+python3.7 ./src/consumer_complaints.py ./input/complaints.csv ./output/report.csv
+```
+The last two arguments should be input and output files, respectively.
+
+### Ways to improve
+1. Implement logging to print errors for a Complaint Id directly to a file rather than the printing to the console itself.
+1. Implement timeouts in while loops in case of column removal. Currently the script will continue to read lines until the last object in the line is an integer and the second to last object either `'Yes', 'No', or 'N/A'` from the Consumer Dispute? column, which has been depreciated since April 24th, 2017.
+
+### Additional comments
+Due to the large file size of the test complaints provided, that file has been omitted from this repository. That file can be found [here](http://files.consumerfinance.gov/ccdb/complaints.csv.zip) if so desired.
+
+This program was created in `python3.7.6` to be exact with the `sys` package.
+
+### Questions?
+Please feel free to email me at remmertjack@gmail.com
+
--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -1,16 +1,44 @@
+"""
+Project: Consumer Complaints
+Author: Jack Remmert
+Date: June 25th, 2020
+
+The objective of this program is, given a comma separated input file of consumer
+complaints (which contains information such as date, company, product, consumer
+notes, company info such as zip code and state, status of complaint)
+output an aggregated file that contains various stats for a 
+product-year combination such as total number of complaints, number of unique
+companies per proudct-year, and the highest percentage of complaints per
+product-year.
+    
+"""
+
 import sys
 
 def product_stats(firm_dict):
     """
+    This function takes in a dictionary, firm_dict, and calculates certain stats.
+    These stats include the total number of complaints received for that product
+    and year, total number of companies receiving at least one complaint for
+    that product and year, and the highest percentage of total complaints
+    filed against one company for that product and year
+    
     Parameters
     ----------
     firm_dict : dictionary
-        DESCRIPTION.
+        Contains a product-year key in the form of xxxx--YYYY and a nested 
+        dictionary with a company key and total number of complaints filed
+        against them for said product and year
 
     Returns
     -------
-    unique_firm_count : dictionary
-        DESCRIPTION.
+    complaints : dictionary
+        Contains product-year key in the form of xxxx--YYYY and a list with the
+        following elements:
+            # total number of complaints
+            # total number of companies receiving at least one complaint (uniqueness)
+            # highest percentage
+            # boolean of 1 or 0 if product-year key contains a double quote (")
 
     """
     complaints = dict()
@@ -35,14 +63,17 @@ def product_stats(firm_dict):
     return complaints
 
 def write_output_file(complaints, output_file):
-    #complaints,unq_firms,hh_per_prod,output_file
     """
+    This functions writes the aggregated data, complaints, to a csv file,
+    output_file.
+    
     Parameters
     ----------
-    input_file : csv
-        DESCRIPTION
+    complaints : dictionary
+        See product_stats complaints for definition
+        
     output_file: csv
-        DESCRIPTION
+        Output file of aggregated data. See beginning of script for example
 
     Returns
     -------
@@ -53,7 +84,7 @@ def write_output_file(complaints, output_file):
     # output_file = r'tests\my_test\output\report.csv'
     with open(output_file, 'w', encoding='UTF-8') as out_f:
         # Write header of the file
-        #out_f.write('product,year,total_complaints,total_companies,highest_percentage\n')
+        # out_f.write('product,year,total_complaints,total_companies,highest_percentage\n')
         
         for product_year in sorted(complaints.items()):
             prod_yr = product_year[0].split('--')
@@ -68,12 +99,20 @@ def write_output_file(complaints, output_file):
            
 def compile_consumer_complaints(input_file, output_file):
     """
+    This is a wrapper function that executes all the steps to compile the 
+    consumer complaints into a csv for the user. It will print every million
+    lines as a progress report and also "All records processed. Total of N
+    records." once eveyr record has been processed. Additionally, once the
+    output csv file, report.csv usually, has been written, the program will
+    print "Complete!" to let the user know the program has finished executing.
+    
     Parameters
     ----------
     input_file : csv
-        DESCRIPTION
+        Input file of data. See beginning of script for example
+        
     output_file: csv
-        DESCRIPTION
+        Output file of aggregated data. See beginning of script for example
 
     Returns
     -------
@@ -95,6 +134,8 @@ def compile_consumer_complaints(input_file, output_file):
                 try:
                     comp_id = record.split(",")[-1].split('\n')[0]
                     dispute = record.split(",")[-2]
+                    # some comments end in a 0-9 value, so look at previous
+                    # value in Consumer Disputed? column, see if in the list
                     if str(dispute) not in ['Yes','No','N/A']:
                         comp_id='not yet'
                     int(comp_id)
@@ -103,12 +144,14 @@ def compile_consumer_complaints(input_file, output_file):
                     record+= inp_f.readline()
             # we need something to split on, yet there can be commas and quotes
             # everywhere.
-            semi_c_record = record.replace(",, ", "; ")
+            semi_c_record = record.replace(",, ", "; ") # spelling mistakes
+            # for all commas in record, but not as the csv, i.e. ,,
             semi_c_record = semi_c_record.replace(", ", "; ")
-            semi_c_record = semi_c_record.replace('\xa0','')
+            semi_c_record = semi_c_record.replace('\xa0','') # for special chars
             semi_c_record = semi_c_record.replace(',",', ';",')
             i=0
             while (i<5):
+                # create our own BREAK for any missing values
                 semi_c_record = semi_c_record.replace(",,",",BREAK,")
                 i+=1
             cl_record = semi_c_record.split(",")
@@ -116,7 +159,8 @@ def compile_consumer_complaints(input_file, output_file):
             year = cl_record[0][:4]
             product = cl_record[1].lower().replace("; ",", ")
             company = cl_record[-11].lower().replace(";",",")
-
+            # minor fixes if the above replacements did not catch a period or
+            # double quote. Simply append the value in above location
             if (company == 'inc.' or company == 'inc' or company == 'inc,'
                 or company =='inc."' or company == 'inc"' or company == 'inc,"'):
                 company = cl_record[-12].lower() +','+ company
@@ -152,8 +196,6 @@ def compile_consumer_complaints(input_file, output_file):
                 else:
                     pass
             record = inp_f.readline()
-            if company == 'â\xa0llc"':
-                print(complaint_id)
             # Progress Report
             num_lines +=1
             if num_lines % 10**6 ==0:
@@ -161,7 +203,7 @@ def compile_consumer_complaints(input_file, output_file):
                       +str(num_lines//10**6)+' million')
                
     print('All Records processed. Total of '
-          +str(num_lines))
+          +str(num_lines)+' records.')
     complaints = product_stats(firms)
     write_output_file(complaints,output_file)
     print('Complete!')

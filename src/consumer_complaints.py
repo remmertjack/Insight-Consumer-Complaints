148578fb35dd25464d3bcad7127e2e1eb7373ef5
remmertjack <remmertjack@gmail.com>
2020-06-26 20:08:50 -0400

    fixed comments

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -81,7 +81,7 @@ def write_output_file(complaints, output_file):
     """
     # Write the output file
     ### FOR TESTING
-    # output_file = r'output/report3.csv'
+    # output_file = r'output/report.csv'
     with open(output_file, 'w', encoding='UTF-8') as out_f:
         # Write header of the file
         # out_f.write('product,year,total_complaints,total_companies,highest_percentage\n')

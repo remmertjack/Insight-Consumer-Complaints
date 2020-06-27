fcf861d056fa1ecfb062f929be41df4bb91f3bb6
remmertjack <remmertjack@gmail.com>
2020-06-27 10:23:20 -0400

    added read function for code readability
    Added, read_and_clean_input_file for better code legability. Instead of having reading and cleaning instead of the wrapper function. Have the wrapper function execute the three functions used to create this process.

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -15,114 +15,38 @@ product-year.
 
 import sys
 
-def product_stats(firm_dict):
+def read_and_clean_input_file(input_file):
     """
-    This function takes in a dictionary, firm_dict, and calculates certain stats.
-    These stats include the total number of complaints received for that product
-    and year, total number of companies receiving at least one complaint for
-    that product and year, and the highest percentage of total complaints
-    filed against one company for that product and year
+    This function reads the data from the input_file, cleans it, and creates a
+    dictionary, firms, that contains every product-year combination as the key
+    and an inner dictionary that consists of a company, as the inner key,
+    and the total number of complaints against said company for the given
+    product-year.
     
-    Parameters
-    ----------
-    firm_dict : dictionary
-        Contains a product-year key in the form of xxxx--YYYY and a nested 
-        dictionary with a company key and total number of complaints filed
-        against them for said product and year
-
-    Returns
-    -------
-    complaints : dictionary
-        Contains product-year key in the form of xxxx--YYYY and a list with the
-        following elements:
-            # total number of complaints
-            # total number of companies receiving at least one complaint (uniqueness)
-            # highest percentage
-            # boolean of 1 or 0 if product-year key contains a double quote (")
-
-    """
-    complaints = dict()
-    for product_year in firm_dict:
-        # find total records for each product and year
-        total = sum(firm_dict[product_year].values())
-        # find unique records for each product and year
-        unq_company_records = len(firm_dict[product_year])
-        # find max percentage for each product and year
-        mx = max(firm_dict[product_year].values())
-        hh_per = int(round(mx/total * 100))
-        # check if key contains quotes...they need to be removed before sorting
-        if ('"' in product_year):
-            include_quotes = 1
-            product_year = product_year.replace('"','')
-        else:
-            include_quotes = 0
-        
-        complaints_record = {product_year:[total, unq_company_records, hh_per,include_quotes]}
-        complaints.update(complaints_record)
-        
-    return complaints
-
-def write_output_file(complaints, output_file):
-    """
-    This functions writes the aggregated data, complaints, to a csv file,
-    output_file.
-    
-    Parameters
-    ----------
-    complaints : dictionary
-        See product_stats complaints for definition
-        
-    output_file: csv
-        Output file of aggregated data. See beginning of script for example
-
-    Returns
-    -------
-    None.
-    """
-    # Write the output file
-    ### FOR TESTING
-    # output_file = r'output/report.csv'
-    with open(output_file, 'w', encoding='UTF-8') as out_f:
-        # Write header of the file
-        # out_f.write('product,year,total_complaints,total_companies,highest_percentage\n')
-        
-        for product_year in sorted(complaints.items()):
-            prod_yr = product_year[0].split('--')
-            if (product_year[-1][-1] == 1):
-                product = '"'+prod_yr[0]+ '"'
-            else:
-                product = prod_yr[0]
-            line = (product+','+prod_yr[1]+','
-                +str(product_year[-1][0]) +','+ str(product_year[-1][1])+','
-                +str(product_year[-1][2]) + '\n')
-            out_f.write(line)
-           
-def compile_consumer_complaints(input_file, output_file):
-    """
-    This is a wrapper function that executes all the steps to compile the 
-    consumer complaints into a csv for the user. It will print every million
-    lines as a progress report and also "All records processed. Total of N
-    records." once eveyr record has been processed. Additionally, once the
-    output csv file, report.csv usually, has been written, the program will
-    print "Complete!" to let the user know the program has finished executing.
+    In the cleaning stages, we clean for commas in any user comments or company
+    names and turn them into semi-colon's. Then we split on the csv provided
+    commas, extract the relevant data and load the data onto our firm dictionary.
     
     Parameters
     ----------
     input_file : csv
-        Input file of data. See beginning of script for example
-        
-    output_file: csv
-        Output file of aggregated data. See beginning of script for example
+        Input file of data. See beginning of script for a summary
 
     Returns
     -------
-    None.
+    firms : dictionary
+        DESCRIPTION.
+    num_lines : int
+        Number of records processed
+    num_errors : int
+        Number of Errors found
 
     """
     firms = dict()
     ### FOR TESTING
     # input_file = r'input/complaints.csv'
     num_lines = 0
+    num_errors = 0
     with open(input_file, 'r', encoding='UTF-8') as inp_f:
         record = inp_f.readline() # header 
         record = inp_f.readline()
@@ -196,7 +120,7 @@ def compile_consumer_complaints(input_file, output_file):
             else:
                 if record != ',,,,,,,,,,,,,,,,,\n' and record !='':
                     print('Error with Complaint ID:' +str(complaint_id))
-                    print(company)
+                    num_errors+=1
                 else:
                     pass
             record = inp_f.readline()
@@ -205,9 +129,124 @@ def compile_consumer_complaints(input_file, output_file):
             if num_lines % 10**6 ==0:
                 print('Number of records processed: '
                       +str(num_lines//10**6)+' million')
-               
-    print('All records processed. Total of '
-          +str(num_lines)+' records.')
+    return firms, num_lines, num_errors
+
+def product_stats(firm_dict):
+    """
+    This function takes in a dictionary, firm_dict, and calculates certain stats.
+    These stats include the total number of complaints received for that product
+    and year, total number of companies receiving at least one complaint for
+    that product and year, and the highest percentage of total complaints
+    filed against one company for that product and year.
+    
+    Parameters
+    ----------
+    firm_dict : dictionary
+        Contains a product-year key in the form of xxxx--YYYY and a nested 
+        dictionary with a company key and total number of complaints filed
+        against them for said product and year.
+
+    Returns
+    -------
+    complaints : dictionary
+        Contains product-year key in the form of xxxx--YYYY and a list with the
+        following elements:
+            # total number of complaints
+            # total number of companies receiving at least one complaint (uniqueness)
+            # highest percentage
+            # boolean of 1 or 0 if product-year key contains a double quote (")
+
+    """
+    complaints = dict()
+    for product_year in firm_dict:
+        # find total records for each product and year
+        total = sum(firm_dict[product_year].values())
+        # find unique records for each product and year
+        unq_company_records = len(firm_dict[product_year])
+        # find max percentage for each product and year
+        mx = max(firm_dict[product_year].values())
+        hh_per = int(round(mx/total * 100))
+        # check if key contains quotes...they need to be removed before sorting
+        if ('"' in product_year):
+            include_quotes = 1
+            product_year = product_year.replace('"','')
+        else:
+            include_quotes = 0
+        
+        complaints_record = {product_year:[total, unq_company_records, hh_per,include_quotes]}
+        complaints.update(complaints_record)
+        
+    return complaints
+
+def write_output_file(complaints, output_file):
+    """
+    This functions writes the aggregated data, complaints, to a csv file,
+    output_file.
+    
+    Parameters
+    ----------
+    complaints : dictionary
+        See product_stats complaints for definition
+        
+    output_file: csv
+        Output file of aggregated data. See beginning of script for example
+
+    Returns
+    -------
+    None.
+    """
+    # Write the output file
+    ### FOR TESTING
+    # output_file = r'output/report.csv'
+    with open(output_file, 'w', encoding='UTF-8') as out_f:
+        # Write header of the file
+        # out_f.write('product,year,total_complaints,total_companies,highest_percentage\n')
+        
+        for product_year in sorted(complaints.items()):
+            prod_yr = product_year[0].split('--')
+            # put back the quotes for a given product, but only if they exist
+            if (product_year[-1][-1] == 1):
+                product = '"'+prod_yr[0]+ '"'
+            else:
+                product = prod_yr[0]
+            line = (product+','+prod_yr[1]+','
+                +str(product_year[-1][0]) +','+ str(product_year[-1][1])+','
+                +str(product_year[-1][2]) + '\n')
+            out_f.write(line)
+           
+def compile_consumer_complaints(input_file, output_file):
+    """
+    This is a wrapper function that executes all the steps to compile the 
+    consumer complaints into a csv for the user.
+    1. It reads and cleans the data with the read_and_clean_input_file
+        function. This function, will print a progress report every million
+        records.
+    2. Print a summary with total number of records and the 
+        number of errors, if they exist. 
+    3. Calcuate the stats with the product_stats function.
+    4. Write the compiled data to the output file.
+    5. Print "Complete!" to let the user know the program has finished executing. 
+    
+    Parameters
+    ----------
+    input_file : csv
+        Input file of data. See beginning of script for a summary
+        
+    output_file: csv
+        Output file of aggregated data. See beginning of script for example
+
+    Returns
+    -------
+    None.
+
+    """
+    firms, num_lines, num_errors = read_and_clean_input_file(input_file)
+    if num_errors == 0:           
+        print('All records processed. Total of '
+              +str(num_lines)+' records. No errors found.')
+    else:
+        print('Records processed. Total of '+str(num_lines)+' records and '
+              +str(num_errors)+' errors found.')
     complaints = product_stats(firms)
     write_output_file(complaints,output_file)
     print('Complete!')
